98b4d46ddeda6ee010ebee493124b99199c1014f
Bianca Miranda <miranda.bianca41@gmail.com>
2022-09-27 20:46:30 -0300

    Armazenamento de Dados Threads

--- src/script.rush.sh
+++ src/script.rush.sh
@@ -4,6 +4,7 @@
 # echo "Quantas vezes $file sera executado?"
 # read j
 for i in 1 2 3 4 5 6 7 8 9 10; do 
-    ./sequencial "../data/matrizGerada1.txt" "../data/matrizGerada2.txt"
+    # ./sequencial "../data/matrizGerada1.txt" "../data/matrizGerada2.txt"
+    ./threads "../data/matrizGerada1.txt" "../data/matrizGerada2.txt" 125000
 	echo "Teste $i"
 done
